8e4eabb97d090e0e83dca728f567dcfc5d4ffe53
sisisin <azsisisin@gmail.com>
2018-08-19 01:42:43 +0900

    format

--- src/lib.js
+++ src/lib.js
@@ -1,70 +1,82 @@
-const promisify = require('util').promisify
-const fs = require('fs')
-const readFileAsync = promisify(fs.readFile)
-const iconv = require('iconv-lite')
+const promisify = require('util').promisify;
+const fs = require('fs');
+const readFileAsync = promisify(fs.readFile);
+const iconv = require('iconv-lite');
 
 async function main() {
-  const csv = await readFromCatalomCsv()
-  console.log(toRenderString(parse(csv)))
+  const csv = await readFromCatalomCsv();
+  console.log(toRenderString(parse(csv)));
 }
 
 function getCsvPath() {
-  if (process.argv.length !== 3) { throw new Error('Runtime error. Invalid command line args.') }
-  if (!process.env['CCSV_DIR']) { throw new Error('Runtime error. Invalid environment variable. "CCSV_DIR" is required.') }
-  return `${process.env['CCSV_DIR']}/${process.argv[2]}.csv`
+  if (process.argv.length !== 3) {
+    throw new Error('Runtime error. Invalid command line args.');
+  }
+  if (!process.env['CCSV_DIR']) {
+    throw new Error('Runtime error. Invalid environment variable. "CCSV_DIR" is required.');
+  }
+  return `${process.env['CCSV_DIR']}/${process.argv[2]}.csv`;
 }
 
 async function readFromCatalomCsv(csvPathArg) {
-  const csvPath = csvPathArg ? csvPathArg : getCsvPath()
-  return iconv.decode(await readFileAsync(csvPath), 'shift_jis')
+  const csvPath = csvPathArg ? csvPathArg : getCsvPath();
+  return iconv.decode(await readFileAsync(csvPath), 'shift_jis');
 }
 
 function parse(rawCsv) {
-  const rows = rawCsv.split('\r\n').map(row => row.split(','))
-  const header = new Header(rows.find(row => row.indexOf('Header') === 0))
+  const rows = rawCsv.split('\r\n').map(row => row.split(','));
+  const header = new Header(rows.find(row => row.indexOf('Header') === 0));
 
   const colors = (() => {
-    const entries = rows
-      .filter(row => row.indexOf('Color') === 0)
-      .map(row => {
-        const c = new Color(Number(row[1]), row[2], row[4]);
-        return [c.colorNumber, c];
-      });
+    const entries = rows.filter(row => row.indexOf('Color') === 0).map(row => {
+      const c = new Color(Number(row[1]), row[2], row[4]);
+      return [c.colorNumber, c];
+    });
     return new Map(entries);
-  })()
+  })();
 
-  const circles = rows.filter(row => row.indexOf('Circle') === 0)
-    .map(row => Circle.parse(row))
+  const circles = rows.filter(row => row.indexOf('Circle') === 0).map(row => Circle.parse(row));
 
-  const unknowns = rows.filter(row => row.indexOf('UnKnown') === 0)
-    .map(row => UnKnown.parse(row))
+  const unknowns = rows.filter(row => row.indexOf('UnKnown') === 0).map(row => UnKnown.parse(row));
 
-  return { header, colors, circles, unknowns }
+  return { header, colors, circles, unknowns };
 }
 
 function toRenderString({ circles, colors }) {
-  return 'color,pre,num,suf,name,author,url,circle_ms_url,memo\n' +
+  return (
+    'color,pre,num,suf,name,author,url,circle_ms_url,memo\n' +
     circles.map(c => toCsvString(c)).join('\n')
+  );
 
   function toCsvString(circle) {
-    return [colors.get(circle.colorNumber).colorName, circle.spacePrefix, circle.spaceNum, circle.spaceSuffix, circle.circleName, circle.authorName, circle.url, circle.circleMsUrl, circle.memo].join(',');
+    return [
+      colors.get(circle.colorNumber).colorName,
+      circle.spacePrefix,
+      circle.spaceNum,
+      circle.spaceSuffix,
+      circle.circleName,
+      circle.authorName,
+      circle.url,
+      circle.circleMsUrl,
+      circle.memo,
+    ].join(',');
   }
 }
 
-class Row { }
+class Row {}
 class Header extends Row {
   constructor(headerItems) {
-    super()
-    this.headerItems = headerItems
+    super();
+    this.headerItems = headerItems;
   }
 }
 
 class Color extends Row {
   constructor(colorNumber, colorCode, colorName) {
-    super()
-    this.colorNumber = colorNumber
-    this.colorCode = colorCode
-    this.colorName = colorName
+    super();
+    this.colorNumber = colorNumber;
+    this.colorCode = colorCode;
+    this.colorName = colorName;
   }
 }
 
@@ -82,36 +94,51 @@ class CircleData extends Row {
       circleMsUrl: arr[23],
       twitterUrl: arr[26],
       pixivUrl: arr[27],
-    }
-    return new this(arg)
+    };
+    return new this(arg);
   }
 
   constructor({
-    colorNumber, spacePrefix, spaceNum, circleName, authorName,
-    url, memo, spaceSuffix, circleMsUrl, twitterUrl, pixivUrl
+    colorNumber,
+    spacePrefix,
+    spaceNum,
+    circleName,
+    authorName,
+    url,
+    memo,
+    spaceSuffix,
+    circleMsUrl,
+    twitterUrl,
+    pixivUrl,
   }) {
-    super()
-    this.colorNumber = colorNumber
-    this.spacePrefix = spacePrefix
-    this.spaceNum = spaceNum
-    this.circleName = circleName
-    this.authorName = authorName
-    this.url = url
-    this.memo = memo
-    this.spaceSuffix = spaceSuffix
-    this.circleMsUrl = circleMsUrl
-    this.twitterUrl = twitterUrl
-    this.pixivUrl = pixivUrl
+    super();
+    this.colorNumber = colorNumber;
+    this.spacePrefix = spacePrefix;
+    this.spaceNum = spaceNum;
+    this.circleName = circleName;
+    this.authorName = authorName;
+    this.url = url;
+    this.memo = memo;
+    this.spaceSuffix = spaceSuffix;
+    this.circleMsUrl = circleMsUrl;
+    this.twitterUrl = twitterUrl;
+    this.pixivUrl = pixivUrl;
   }
 }
 
-class Circle extends CircleData { }
-class UnKnown extends CircleData { }
+class Circle extends CircleData {}
+class UnKnown extends CircleData {}
 
 module.exports = {
   // for application
-  main, 
+  main,
 
   // for tests
-  readFromCatalomCsv, parse, Color, Header, Circle, UnKnown, toRenderString
-}
+  readFromCatalomCsv,
+  parse,
+  Color,
+  Header,
+  Circle,
+  UnKnown,
+  toRenderString,
+};
--- test/e2e-test.js
+++ test/e2e-test.js
@@ -1,12 +1,13 @@
-const expect = require('chai').expect
-const { readFromCatalomCsv, parse, toRenderString } = require('../src/lib')
+const expect = require('chai').expect;
+const { readFromCatalomCsv, parse, toRenderString } = require('../src/lib');
 
 describe('e2e', () => {
   it('should get csv string', async () => {
-    const csv = await readFromCatalomCsv('./test/csvs/sample.csv')
-    const csvString = toRenderString(parse(csv))
-    expect(csvString).to.eq(`color,space_prefix,space_num,space_suffix,circle_name,author_name,url,circle_ms_url
+    const csv = await readFromCatalomCsv('./test/csvs/sample.csv');
+    const csvString = toRenderString(parse(csv));
+    expect(csvString).to
+      .eq(`color,space_prefix,space_num,space_suffix,circle_name,author_name,url,circle_ms_url
 1,Ａ,1,a,おでんランチ。,あむぱか,http://pixiv.me/otuok_panda_mp,https://twitter.com/amupaka
-1,Ａ,27,a,スタヂオK-ing,,http://www.sam.hi-ho.ne.jp/kimura/,`)
-  })
-})
+1,Ａ,27,a,スタヂオK-ing,,http://www.sam.hi-ho.ne.jp/kimura/,`);
+  });
+});
